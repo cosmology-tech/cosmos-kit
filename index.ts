8996580d8cacc5918ca1a2f2f6887f97c2fead26
Noah Saso <noahsaso@gmail.com>
2022-05-11 23:42:53 -0700

    Fill in undefined browser.storage as a temp Safari fix.

--- index.ts
+++ index.ts
@@ -1,2 +1,19 @@
+// Fix Safari's nonexistent browser.storage https://github.com/chainapsis/keplr-wallet/blob/4726a96b9663f17b91c5d6b0448bf85ebb4a678a/packages/common/src/kv-store/extension.ts
+
+declare namespace browser.storage {
+  const local: {
+    get: undefined;
+    set: undefined;
+  };
+}
+
+if (
+  typeof window !== "undefined" &&
+  typeof browser !== "undefined" &&
+  typeof browser.storage === "undefined"
+) {
+  browser.storage = { local: { get: undefined, set: undefined } };
+}
+
 export * from "./components";
 export * from "./providers";
