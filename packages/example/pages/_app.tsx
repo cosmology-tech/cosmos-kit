d42053bfd4e9447914227abb08e257547253fa41
Eliot Baker <430383+ebaker@users.noreply.github.com>
2022-07-18 10:54:41 -0700

    copy example from @noahsaso/cosmodal example

---
+++ packages/example/pages/_app.tsx
@@ -0,0 +1,48 @@
+import '../styles/globals.css'
+
+import {
+  ChainInfoID,
+  WalletManagerProvider,
+  WalletType,
+} from '@cosmos-wallet/react'
+import type { AppProps } from 'next/app'
+import { FunctionComponent } from 'react'
+import { GasPrice } from '@cosmjs/stargate'
+
+const LOCAL_STORAGE_KEY = 'connectedWalletId'
+
+const MyApp: FunctionComponent<AppProps> = ({ Component, pageProps }) => (
+  <WalletManagerProvider
+    walletConnectClientMeta={{
+      name: 'CosmodalExampleDApp',
+      description: 'A dApp using the @noahsaso/cosmodal library.',
+      url: 'https://noahsaso-cosmodal.vercel.app',
+      icons: ['https://moonphase.is/image.svg'],
+    }}
+    enabledWalletTypes={[WalletType.Keplr, WalletType.WalletConnectKeplr]}
+    renderLoader={() => <p>Loading...</p>}
+    localStorageKey={LOCAL_STORAGE_KEY}
+    defaultChainId={ChainInfoID.Juno1}
+    getSigningCosmWasmClientOptions={(chainInfo) => ({
+      gasPrice: GasPrice.fromString(
+        '0.0025' + chainInfo.feeCurrencies[0].coinMinimalDenom
+      ),
+    })}
+    getSigningStargateClientOptions={(chainInfo) => ({
+      gasPrice: GasPrice.fromString(
+        '0.0025' + chainInfo.feeCurrencies[0].coinMinimalDenom
+      ),
+    })}
+    // Choose a different RPC node for the desired chain.
+    // chainInfoOverrides={[
+    //   {
+    //     ...ChainInfoMap[ChainInfoID.Juno1],
+    //     rpc: "https://another.rpc.com",
+    //   }
+    // ]}
+  >
+    <Component {...pageProps} />
+  </WalletManagerProvider>
+)
+
+export default MyApp
