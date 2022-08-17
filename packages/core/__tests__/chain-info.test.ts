c363e43f7fcc4ab9506a67cbd4c7d273470122ae
Dan Lynch <pyramation@gmail.com>
2022-08-17 16:40:15 -0700

    tests and snaps

--- packages/core/__tests__/chain-info.test.ts
+++ packages/core/__tests__/chain-info.test.ts
@@ -1,21 +1,34 @@
-import { getChainInfo } from '../src/chainInfo';
+import { getKeplrChainInfoLegacy } from '../src/keplrChainInfo';
+import { getKeplrChainInfo } from '../src/chainInfo';
+import { chains, assets } from 'chain-registry'
+
+const testChainData = async (chainName, chainId) => {
+    const old = await getKeplrChainInfoLegacy(chainId);
+    expect(old).toMatchSnapshot();
+    const chain = await getKeplrChainInfo(chainName, chains, assets);
+    expect(chain).toMatchSnapshot();
+};
 
 it('osmosis', async () => {
-    const chain = await  getChainInfo('osmosis-1');
-    expect(chain).toMatchSnapshot();
+    const chainName = 'osmosis';
+    const chainId = 'osmosis-1';
+    await testChainData(chainName, chainId);
 })
 
 it('gravity', async () => {
-    const chain = await  getChainInfo('gravity-bridge-3');
-    expect(chain).toMatchSnapshot();
+    const chainName = 'gravitybridge';
+    const chainId = 'gravity-bridge-3';
+    await testChainData(chainName, chainId);
 })
 
 it('persistence', async () => {
-    const chain = await  getChainInfo('core-1');
-    expect(chain).toMatchSnapshot();
+    const chainName = 'persistence';
+    const chainId = 'core-1';
+    await testChainData(chainName, chainId);
 })
 
 it('kava', async () => {
-    const chain = await  getChainInfo('kava_2222-10');
-    expect(chain).toMatchSnapshot();
+    const chainName = 'kava';
+    const chainId = 'kava_2222-10';
+    await testChainData(chainName, chainId);
 })
