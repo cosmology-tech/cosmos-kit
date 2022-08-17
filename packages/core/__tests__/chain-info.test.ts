ff0048b770efa58b1e2149d6c4402aa78adbb9fd
Dan Lynch <pyramation@gmail.com>
2022-08-17 14:48:44 -0700

    snaps for other chains

--- packages/core/__tests__/chain-info.test.ts
+++ packages/core/__tests__/chain-info.test.ts
@@ -1,6 +1,21 @@
 import { getChainInfo } from '../src/chainInfo';
 
-it('works', async () => {
+it('osmosis', async () => {
     const chain = await  getChainInfo('osmosis-1');
+    expect(chain).toMatchSnapshot();
+})
+
+it('gravity', async () => {
+    const chain = await  getChainInfo('gravity-bridge-3');
+    expect(chain).toMatchSnapshot();
+})
+
+it('persistence', async () => {
+    const chain = await  getChainInfo('core-1');
+    expect(chain).toMatchSnapshot();
+})
+
+it('kava', async () => {
+    const chain = await  getChainInfo('kava_2222-10');
     expect(chain).toMatchSnapshot();
 })
