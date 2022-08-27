e1b2f0a9bf92a86191b6a0c1214fa92d55820981
besated <bryan@falconwallet.app>
2022-08-26 17:26:14 -0700

    Add falcon to the wallets registry

--- packages/registry/src/wallets.ts
+++ packages/registry/src/wallets.ts
@@ -1,4 +1,9 @@
+import { FalconWallet } from '@cosmos-kit/falcon'
 import { KeplrWallet, KeplrWalletConnectWallet } from '@cosmos-kit/keplr'
 import { Wallet } from '@cosmos-kit/types'
 
-export const Wallets: Wallet[] = [KeplrWallet, KeplrWalletConnectWallet]
+export const Wallets: Wallet[] = [
+  KeplrWallet,
+  KeplrWalletConnectWallet,
+  FalconWallet,
+]
