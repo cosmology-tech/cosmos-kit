dacd1e27017f12ef0b6688b5b4ee833cf81618dc
JunLiu93 <853480121@qq.com>
2022-09-17 12:00:37 +0800

    modify compile and package configs

--- packages/core/.babelrc.js
+++ packages/core/.babelrc.js
@@ -3,14 +3,15 @@ const useESModules = !!process.env.MODULE;
 module.exports = (api) => {
   api.cache(() => process.env.MODULE);
   return {
+    presets: useESModules
+    ? [["@babel/preset-typescript", { allowDeclareFields: true }]]
+    : [["@babel/preset-typescript", { allowDeclareFields: true }], "@babel/preset-env"],
     plugins: [
+      ['@babel/plugin-transform-typescript', { allowDeclareFields: true }],
       ['@babel/transform-runtime', { useESModules }],
       '@babel/proposal-object-rest-spread',
       '@babel/proposal-class-properties',
       '@babel/proposal-export-default-from'
     ],
-    presets: useESModules
-      ? [["@babel/typescript", { allowDeclareFields: true }]]
-      : [["@babel/typescript", { allowDeclareFields: true }], "@babel/env"],
   };
 };
