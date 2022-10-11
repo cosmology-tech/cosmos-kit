b6a833e66f56f94ad99bb46689516ce244b9b0d2
JunLiu93 <853480121@qq.com>
2022-10-11 10:50:12 +0800

    customize wallet info

--- packages/example/pages/_app.tsx
+++ packages/example/pages/_app.tsx
@@ -5,8 +5,14 @@ import { defaultTheme } from '../config';
 
 import { chains, assets } from 'chain-registry';
 import { Chain } from '@chain-registry/types';
-import { wallets } from '@cosmos-kit/keplr';
+// import { wallets } from '@cosmos-kit/keplr';
+import { KeplrExtensionWallet, KeplrMobileWallet } from '@cosmos-kit/keplr';
+import { keplrExtensionInfo, keplrMobileInfo } from '@cosmos-kit/keplr';
 
+const keplrExtension = new KeplrExtensionWallet(keplrExtensionInfo);
+const KeplrMobile = new KeplrMobileWallet(keplrMobileInfo);
+
+export const wallets = [keplrExtension, KeplrMobile];
 
 function MyApp({ Component, pageProps }: AppProps) {
 
