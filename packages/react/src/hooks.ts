a153c105e56b22ec3549c765699da1a4ad04cde3
liujun93 <ljun8575@gmail.com>
2022-11-15 17:09:25 +0800

    test-yarn

--- packages/react/src/hooks.ts
+++ packages/react/src/hooks.ts
@@ -6,7 +6,7 @@ import { walletContext } from './provider';
 export const useWallet = (): WalletManager => {
   const context = React.useContext(walletContext);
 
-  // test-yarn
+  // test-yarn test-yarn
   if (!context || !context.walletManager) {
     throw new Error('You have forgot to use WalletProvider');
   }
