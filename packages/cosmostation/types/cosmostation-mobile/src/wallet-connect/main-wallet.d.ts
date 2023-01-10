91d938f053e2730f6b92e81222ef651ae42e5af5
TrevorJTClarke <trevorjtclarke@gmail.com>
2023-01-10 12:19:50 -0800

    Package json cleanup for dependencies

--- packages/cosmostation/types/cosmostation-mobile/src/wallet-connect/main-wallet.d.ts
+++
@@ -1,5 +0,0 @@
-import { Wallet } from '@cosmos-kit/core';
-import { WCWalletV1 } from '@cosmos-kit/walletconnect-v1';
-export declare class CosmostationMobileWallet extends WCWalletV1 {
-    constructor(walletInfo: Wallet);
-}
--- packages/trust/types/trust/src/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { TrustExtensionWallet } from '@cosmos-kit/trust-extension';
-export declare const wallets: TrustExtensionWallet[];
-export declare const walletNames: string[];
