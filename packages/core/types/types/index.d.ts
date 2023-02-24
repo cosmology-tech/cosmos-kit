1ea14dc253447454598f8ad62211f0af66658245
liujun93 <ljun8575@gmail.com>
2023-02-24 12:24:25 +0800

    types

---
+++ packages/core/types/types/index.d.ts
@@ -0,0 +1,6 @@
+export * from './chain';
+export * from './common';
+export * from './manager';
+export * from './wallet';
+export * from './hook';
+export * from './view';
---
+++ wallets/cosmostation/types/index.d.ts
@@ -0,0 +1,1 @@
+export declare const wallets: (import("@cosmos-kit/cosmostation-extension").CosmostationExtensionWallet | import("@cosmos-kit/cosmostation-mobile").CosmostationMobileWallet)[];
