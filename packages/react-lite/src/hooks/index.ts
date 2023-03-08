6394db1632c82f1856f0c5771a495d9725e62e7b
liujun93 <ljun8575@gmail.com>
2023-03-08 10:35:27 +0800

    move useModalTheme

--- packages/react-lite/src/hooks/index.ts
+++ packages/react-lite/src/hooks/index.ts
@@ -1,6 +1,5 @@
 export * from './useChain';
 export * from './useManager';
 export * from './useNameService';
-export * from './useModalTheme';
 export * from './useWallet';
 export * from './useWalletClient';
--- packages/react/types/index.d.ts
+++ packages/react/types/index.d.ts
@@ -2,3 +2,4 @@ export * from '@cosmos-kit/react-lite/src/hooks';
 export { walletContext } from '@cosmos-kit/react-lite';
 export * from './modal';
 export * from './provider';
+export * from './hooks';
