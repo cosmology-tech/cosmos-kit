b2ffee5a52fe627e38ebdb6cdccd549c81581a04
liujun93 <ljun8575@gmail.com>
2023-03-15 13:17:41 +0800

    types

---
+++ packages/core/types/types/manager.d.ts
@@ -0,0 +1,38 @@
+import { Chain } from '@chain-registry/types';
+import { SigningCosmWasmClientOptions } from '@cosmjs/cosmwasm-stargate';
+import { SigningStargateClientOptions, StargateClientOptions } from '@cosmjs/stargate';
+import { ChainName } from './chain';
+import { Dispatch, SignType, StateActions } from './common';
+import { WalletName } from './wallet';
+export interface SignerOptions {
+    stargate?: (chain: Chain) => StargateClientOptions | undefined;
+    signingStargate?: (chain: Chain) => SigningStargateClientOptions | undefined;
+    signingCosmwasm?: (chain: Chain) => SigningCosmWasmClientOptions | undefined;
+    preferredSignType?: (chain: Chain) => SignType | undefined;
+}
+export interface ViewOptions {
+    alwaysOpenView?: boolean;
+    closeViewWhenWalletIsConnected?: boolean;
+    closeViewWhenWalletIsDisconnected?: boolean;
+    closeViewWhenWalletIsRejected?: boolean;
+}
+export interface StorageOptions {
+    disabled?: boolean;
+    duration?: number;
+    clearOnTabClose?: boolean;
+}
+export interface SessionOptions {
+    duration: number;
+    callback?: () => void;
+}
+export interface Endpoints {
+    rpc?: string[];
+    rest?: string[];
+}
+export declare type EndpointOptions = Record<ChainName, Endpoints>;
+export interface ManagerActions<T> extends StateActions<T> {
+    walletName?: Dispatch<WalletName | undefined>;
+    chainName?: Dispatch<ChainName | undefined>;
+    viewOpen?: Dispatch<boolean>;
+}
+export declare type EventName = 'refresh_connection';
---
+++ wallets/keplr-extension/types/extension/client.d.ts
@@ -0,0 +1,28 @@
+import { StdSignDoc } from '@cosmjs/amino';
+import { Algo, OfflineDirectSigner } from '@cosmjs/proto-signing';
+import { ChainRecord, DirectSignDoc, SignOptions, SignType, WalletClient } from '@cosmos-kit/core';
+import { BroadcastMode, Keplr } from '@keplr-wallet/types';
+export declare class KeplrClient implements WalletClient {
+    readonly client: Keplr;
+    constructor(client: Keplr);
+    enable(chainIds: string | string[]): Promise<void>;
+    getSimpleAccount(chainId: string): Promise<{
+        namespace: string;
+        chainId: string;
+        address: string;
+        username: string;
+    }>;
+    getAccount(chainId: string): Promise<{
+        username: string;
+        address: string;
+        algo: Algo;
+        pubkey: Uint8Array;
+    }>;
+    getOfflineSigner(chainId: string, preferredSignType?: SignType): OfflineDirectSigner | import("@keplr-wallet/types").OfflineAminoSigner;
+    getOfflineSignerAmino(chainId: string): import("@keplr-wallet/types").OfflineAminoSigner;
+    getOfflineSignerDirect(chainId: string): OfflineDirectSigner;
+    addChain(chainInfo: ChainRecord): Promise<void>;
+    signAmino(chainId: string, signer: string, signDoc: StdSignDoc, signOptions?: SignOptions): Promise<import("@keplr-wallet/types").AminoSignResponse>;
+    signDirect(chainId: string, signer: string, signDoc: DirectSignDoc, signOptions?: SignOptions): Promise<import("@keplr-wallet/types").DirectSignResponse>;
+    sendTx(chainId: string, tx: Uint8Array, mode: BroadcastMode): Promise<Uint8Array>;
+}
