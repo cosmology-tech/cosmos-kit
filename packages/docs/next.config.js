4aa5a53f92eb780dc814a983452601ce3f9d8bc0
liujun93 <ljun8575@gmail.com>
2023-04-17 23:45:00 +0800

    update

--- packages/docs/next.config.js
+++ packages/docs/next.config.js
@@ -1,41 +1,59 @@
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.js',
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.js",
   unstable_staticImage: true,
   unstable_flexsearch: {
-    codeblock: false
-  }
-})
+    codeblock: false,
+  },
+});
 
 module.exports = {
   ...withNextra({
-  reactStrictMode: true,
-  webpack: (
-    config,
-    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
-  ) => {
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      use: [
+    reactStrictMode: true,
+    async redirects() {
+      return [
         {
-          loader: "babel-loader",
-          options: {
-            presets: [
-              "@babel/preset-env",
-              ["@babel/preset-react", {"runtime": "automatic"}],
-              ["@babel/typescript", { allowDeclareFields: true }],
-            ],
-          },
+          source: "/",
+          destination: "/v1",
+          permanent: false,
         },
-      ],
-    });
+        {
+          source: "/index",
+          destination: "/v1",
+          permanent: false,
+        },
+      ];
+    },
+    webpack: (
+      config,
+      { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+    ) => {
+      config.module.rules.push({
+        test: /\.(ts|tsx)$/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                "@babel/preset-env",
+                ["@babel/preset-react", { runtime: "automatic" }],
+                ["@babel/typescript", { allowDeclareFields: true }],
+              ],
+            },
+          },
+        ],
+      });
 
-    config.resolve.extensions.push('.ts', '.tsx');
-    return config;
-  },
+      config.resolve.extensions.push(".ts", ".tsx");
+      return config;
+    },
   }),
   images: {
-    domains: ['user-images.githubusercontent.com', 'github.com', 'img.shields.io'],
-    unoptimized: true
-  }
-}
+    domains: [
+      "user-images.githubusercontent.com",
+      "github.com",
+      "img.shields.io",
+    ],
+    unoptimized: true,
+  },
+};
