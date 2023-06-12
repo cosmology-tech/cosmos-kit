5fd38f5d8bc04d425ecfe31534a32f3a7a2062cd
Tom Wang <tom.wanghaiyi@gmail.com>
2023-06-12 21:53:08 +0800

    Build images for docs and example

--- packages/docs/next.config.js
+++ packages/docs/next.config.js
@@ -9,30 +9,33 @@ const withNextra = require('nextra')({
 
 module.exports = {
   ...withNextra({
-  reactStrictMode: true,
-  webpack: (
-    config,
-    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
-  ) => {
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      use: [
-        {
-          loader: "babel-loader",
-          options: {
-            presets: [
-              "@babel/preset-env",
-              ["@babel/preset-react", {"runtime": "automatic"}],
-              ["@babel/typescript", { allowDeclareFields: true }],
-            ],
+    reactStrictMode: true,
+    webpack: (
+      config,
+      { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+    ) => {
+      config.module.rules.push({
+        test: /\.(ts|tsx)$/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                "@babel/preset-env",
+                ["@babel/preset-react", { "runtime": "automatic" }],
+                ["@babel/typescript", { allowDeclareFields: true }],
+              ],
+              plugins: [
+                'inline-import-data-uri'
+              ]
+            },
           },
-        },
-      ],
-    });
+        ],
+      });
 
-    config.resolve.extensions.push('.ts', '.tsx');
-    return config;
-  },
+      config.resolve.extensions.push('.ts', '.tsx');
+      return config;
+    },
   }),
   images: {
     domains: ['user-images.githubusercontent.com', 'github.com', 'img.shields.io'],
