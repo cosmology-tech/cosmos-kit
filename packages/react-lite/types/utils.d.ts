04e4b6336095244f911b3745e6d31351dbbbb608
yyyyaaa <phatghaa@gmail.com>
2023-06-20 21:14:28 +0700

    fix: babel output not compatible

--- packages/react-lite/types/utils.d.ts
+++
@@ -1,2 +0,0 @@
-import { ChainWalletBase, ChainWalletContext } from '@cosmos-kit/core';
-export declare function getChainWalletContext(chainId: string, wallet?: ChainWalletBase, sync?: boolean): ChainWalletContext;
