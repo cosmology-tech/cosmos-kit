2d8a21fdc210e9bc8dbeb8b9401cfb37c547a4f1
yyyyaaa <phatghaa@gmail.com>
2023-06-20 21:14:28 +0700

    fix: babel output not compatible

--- packages/react-lite/types/hooks/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './useChain';
-export * from './useChainWallet';
-export * from './useManager';
-export * from './useNameService';
-export * from './useWallet';
-export * from './useWalletClient';
--- packages/react-lite/types/hooks/useChain.d.ts
+++
@@ -1,2 +0,0 @@
-import { ChainContext, ChainName } from '@cosmos-kit/core';
-export declare const useChain: (chainName: ChainName, sync?: boolean) => ChainContext;
