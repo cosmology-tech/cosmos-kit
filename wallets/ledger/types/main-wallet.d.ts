8b6a82e0eb70044f974154c75e5a5b8e5132136e
liujun93 <ljun8575@gmail.com>
2023-06-30 19:45:03 +0800

    update

--- wallets/ledger/types/main-wallet.d.ts
+++
@@ -1,8 +0,0 @@
-import { EndpointOptions, Wallet } from '@cosmos-kit/core';
-import { MainWalletBase } from '@cosmos-kit/core';
-import { TransportType } from './utils';
-export declare class LedgerMainWallet extends MainWalletBase {
-    transportType: TransportType;
-    constructor(walletInfo: Wallet, preferredEndpoints?: EndpointOptions['endpoints'], transportType?: TransportType);
-    initClient(): Promise<void>;
-}
--- wallets/shell-extension/types/extension/main-wallet.d.ts
+++
@@ -1,6 +0,0 @@
-import { EndpointOptions, Wallet } from '@cosmos-kit/core';
-import { MainWalletBase } from '@cosmos-kit/core';
-export declare class ShellExtensionWallet extends MainWalletBase {
-    constructor(walletInfo: Wallet, preferredEndpoints?: EndpointOptions['endpoints']);
-    initClient(): Promise<void>;
-}
