56fdfa3cb721b4abb23f311fb82a7b0f6387a2dc
liujun93 <ljun8575@gmail.com>
2023-07-07 12:27:54 +0800

    next config

--- packages/example/next.config.js
+++ packages/example/next.config.js
@@ -2,10 +2,12 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
-const withTM = require('next-transpile-modules')(['@cosmos-kit/web3auth']);
+const withTM = require("next-transpile-modules")(["@cosmos-kit/web3auth"]);
 
 /** @type {import('next').NextConfig} */
-module.exports = withTM(withBundleAnalyzer({
-  reactStrictMode: true,
-  swcMinify: false,
-});
+module.exports = withTM(
+  withBundleAnalyzer({
+    reactStrictMode: true,
+    swcMinify: false,
+  })
+);
