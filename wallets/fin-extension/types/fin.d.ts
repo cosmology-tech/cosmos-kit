408c75144ce7b21fbadb073fbc7acdce5808c35f
liujun93 <ljun8575@gmail.com>
2023-07-18 09:50:47 +0800

    delete types

--- wallets/fin-extension/types/fin.d.ts
+++
@@ -1,2 +0,0 @@
-import { FinExtensionWallet } from './extension';
-export declare const wallets: FinExtensionWallet[];
--- wallets/fin-extension/types/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './extension';
-export * from './extension/client';
-export * from './fin';
