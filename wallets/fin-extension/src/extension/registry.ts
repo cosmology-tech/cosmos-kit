8dbfa39c8fe30599c771b526e8dd66471f674fe8
khiêm <87562184+khiemsoft@users.noreply.github.com>
2023-08-31 15:08:36 +0700

    Update fin naming and event names

--- wallets/fin-extension/src/extension/registry.ts
+++ wallets/fin-extension/src/extension/registry.ts
@@ -1,7 +1,7 @@
 import { Wallet } from '@cosmos-kit/core';
 
 export const FinExtensionInfo: Wallet = {
-  name: 'fin',
+  name: 'fin-extension',
   logo: 'https://lh3.googleusercontent.com/oKvetnosutN7Q7daICbew7k9kLLB1unNlggkA-mZZZZIdNWp7vJFpz5q9MZ73Wlw5S5KrHV5mEQ0UEKYaDWz5qmacHw=w128-h128-e365-rj-sc0x00ffffff',
   prettyName: 'Fin',
   mode: 'extension',
@@ -10,6 +10,10 @@ export const FinExtensionInfo: Wallet = {
   rejectMessage: {
     source: 'Request rejected',
   },
+  connectEventNamesOnWindow: [
+    'accountsChanged', 
+    'networkChanged',
+  ],
   downloads: [
     {
       device: 'desktop',
