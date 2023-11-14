2c6ed552d30edabae4a9a6a5c86973d008dd62b8
yyyyaaa <phatghaa@gmail.com>
2023-11-14 12:20:03 +0700

    fix: add docs

--- packages/docs/next.config.js
+++ packages/docs/next.config.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
@@ -10,28 +11,43 @@ const withNextra = require("nextra")({
 module.exports = {
   ...withNextra({
     reactStrictMode: true,
-    webpack: (
-      config,
-      { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
-    ) => {
-      config.module.rules.push({
-        test: /\.(ts|tsx)$/,
-        use: [
-          {
-            loader: "babel-loader",
-            options: {
-              presets: [
-                "@babel/preset-env",
-                ["@babel/preset-react", { runtime: "automatic" }],
-                ["@babel/typescript", { allowDeclareFields: true }],
-              ],
-              plugins: ["inline-import-data-uri"],
-            },
-          },
-        ],
-      });
+    // webpack: (
+    //   config,
+    //   { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+    // ) => {
+    //   config.module.rules.push({
+    //     test: /\.(ts|tsx)$/,
+    //     use: [
+    //       {
+    //         loader: "babel-loader",
+    //         options: {
+    //           presets: [
+    //             "@babel/preset-env",
+    //             ["@babel/preset-react", { runtime: "automatic" }],
+    //             ["@babel/typescript", { allowDeclareFields: true }],
+    //           ],
+    //           plugins: ["inline-import-data-uri"],
+    //         },
+    //       },
+    //     ],
+    //   });
 
-      config.resolve.extensions.push(".ts", ".tsx");
+    //   config.resolve.extensions.push(".ts", ".tsx");
+    //   return config;
+    // },
+    /**
+     *
+     * @param {import('webpack').Configuration} config
+     * @param {import('next/dist/server/config-shared').WebpackConfigContext} _context
+     * @returns {import('webpack').Configuration}
+     */
+    webpack: (config, _context) => {
+      const overridePath = path.resolve(
+        __dirname,
+        "../..",
+        "node_modules/react"
+      );
+      config.resolve.alias["react"] = overridePath;
       return config;
     },
   }),
