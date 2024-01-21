46612fcdf7314b9b0584a1c2de4ba271b4f0a339
Dan Lynch <pyramation@gmail.com>
2024-01-20 16:01:01 -0800

    props

--- packages/cosmos-kit/src/index.ts
+++ packages/cosmos-kit/src/index.ts
@@ -3,9 +3,13 @@ export { wallets } from './wallets';
 import {
     SignerOptions,
     ChainName,
+    WalletStatus,
+    WalletModalProps
 } from '@cosmos-kit/core';
 
 export type {
+    WalletModalProps,
+    WalletStatus,
     ChainName,
     SignerOptions
 };
