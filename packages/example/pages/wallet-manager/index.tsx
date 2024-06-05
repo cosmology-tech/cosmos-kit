819851ed8b3e6fe11eb948685b33af67a054cbe6
Lucas Jiang <secretjiang@gmail.com>
2024-06-05 21:08:33 +0800

    restructure folder

---
+++ packages/example/pages/wallet-manager/index.tsx
@@ -0,0 +1,11 @@
+export default () => {
+  return (
+    <ul>
+      <li><a href='/wallet-manager/wallet-operate'>Operate Wallet Directly</a></li>
+      <li><a href='/wallet-manager/main-wallets'>Show All Main Wallet</a></li>
+      <li><a href='/wallet-manager/wallet-manager'>Wallet Manager Without Hook</a></li>
+      <li><a href='/wallet-manager/wallet-repos'>Show All Wallet Repo</a></li>
+      <li><a href='/wallet-manager/wallet-repo'>Show One Wallet Repo</a></li>
+    </ul>
+  )
+}
