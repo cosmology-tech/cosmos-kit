5a00a9180f07f9a85ac78427ee379b1e22877826
Dan Lynch <pyramation@gmail.com>
2024-06-18 12:37:05 -0700

    lint

--- wallets/leap-capsule-social-login/src/index.ts
+++ wallets/leap-capsule-social-login/src/index.ts
@@ -1,5 +1,5 @@
 export * from './chain-wallet';
-export * from './constant';
-export * from './main-wallet';
 export * from './client';
+export * from './constant';
 export * from './leap-social';
+export * from './main-wallet';
