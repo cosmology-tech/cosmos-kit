5e78eda716f3bcfd5eac18df76181c63bdaaf0ce
Joe S <schnetzlerjoe@gmail.com>
2024-08-01 16:08:48 -0400

    Revert cosmostation

--- wallets/cosmostation-extension/src/extension/client.ts
+++ wallets/cosmostation-extension/src/extension/client.ts
@@ -1,20 +1,27 @@
-import { OfflineAminoSigner, StdSignature, StdSignDoc } from '@cosmjs/amino';
-import { Algo, DirectSignResponse, OfflineDirectSigner } from '@cosmjs/proto-signing';
-import { ChainRecord, DirectSignDoc, SignType } from '@cosmos-kit/core';
-import { SignOptions, WalletClient } from '@cosmos-kit/core';
+import { chainRegistryChainToCosmostation } from '@chain-registry/cosmostation';
+import { StdSignature, StdSignDoc } from '@cosmjs/amino';
 import {
-  Chain,
-  CosmosSnap,
-  installSnap,
-  signDirect,
-  signAmino,
-  suggestChain,
-} from '@cosmsnap/snapper';
-import { SignDoc } from 'cosmjs-types/cosmos/tx/v1beta1/tx';
-import Long from 'long';
+  BroadcastMode,
+  ChainRecord,
+  DirectSignDoc,
+  ExtendedHttpEndpoint,
+  SignOptions,
+  SignType,
+  SuggestToken,
+  WalletClient,
+} from '@cosmos-kit/core';
 
-export class CosmosExtensionClient implements WalletClient {
-  cosmos: CosmosSnap;
+import { Cosmostation, RequestAccountResponse } from './types';
+import Long from 'long';
+import { DirectSignResponse } from '@cosmjs/proto-signing';
+import { SignDoc } from 'cosmjs-types/cosmos/tx/v1beta1/tx';
+
+export class CosmostationClient implements WalletClient {
+  readonly client: Cosmostation;
+  private eventMap: Map<
+    string,
+    Map<EventListenerOrEventListenerObject, Event>
+  > = new Map();
   private _defaultSignOptions: SignOptions = {
     preferNoSetFee: false,
     preferNoSetMemo: true,
@@ -29,31 +36,72 @@ export class CosmosExtensionClient implements WalletClient {
     this._defaultSignOptions = options;
   }
 
-  constructor() {
-    this.cosmos = new CosmosSnap();
+  constructor(client: Cosmostation) {
+    this.client = client;
   }
 
-  async addChain(chainInfo: ChainRecord) {
-    await suggestChain(chainInfo.chain as unknown as Chain);
+  get cosmos() {
+    return this.client.cosmos;
+  }
+
+  get ikeplr() {
+    return this.client.providers.keplr;
+  }
+
+  async suggestToken({ chainName, tokens, type }: SuggestToken) {
+    if (type === 'cw20') {
+      await this.cosmos.request({
+        method: 'cos_addTokensCW20',
+        params: {
+          chainName,
+          tokens,
+        },
+      });
+    }
   }
 
   async getSimpleAccount(chainId: string) {
-    const { address } = await this.getAccount(chainId);
+    const { address, username } = await this.getAccount(chainId);
     return {
       namespace: 'cosmos',
       chainId,
       address,
+      username,
     };
   }
 
   async getAccount(chainId: string) {
-    await installSnap();
-    const key = await this.cosmos.getAccount(chainId);
+    const key = (await this.cosmos.request({
+      method: 'cos_requestAccount',
+      params: { chainName: chainId },
+    })) as RequestAccountResponse;
     return {
+      username: key.name,
       address: key.address,
-      algo: key.algo as Algo,
-      pubkey: key.pubkey,
+      pubkey: key.publicKey,
+      algo: key.algo,
     };
+  }
+
+  async disconnect() {
+    await this.cosmos.request({
+      method: 'cos_disconnect',
+    });
+  }
+
+  on(type: string, listener: EventListenerOrEventListenerObject): void {
+    const event = this.cosmos.on(type, listener);
+    const typeEventMap: Map<EventListenerOrEventListenerObject, Event> =
+      this.eventMap.get(type) || new Map();
+    typeEventMap.set(listener, event);
+    this.eventMap.set(type, typeEventMap);
+  }
+
+  off(type: string, listener: EventListenerOrEventListenerObject): void {
+    const event = this.eventMap.get(type)?.get(listener);
+    if (event) {
+      this.cosmos.off(event);
+    }
   }
 
   getOfflineSigner(chainId: string, preferredSignType?: SignType) {
@@ -65,14 +113,40 @@ export class CosmosExtensionClient implements WalletClient {
       default:
         return this.getOfflineSignerAmino(chainId);
     }
+    // return this.ikeplr.getOfflineSignerAuto(chainId);
   }
 
   getOfflineSignerAmino(chainId: string) {
-    return this.getOfflineSigner(chainId, "amino") as unknown as OfflineAminoSigner;
+    return this.ikeplr.getOfflineSignerOnlyAmino(chainId);
   }
 
   getOfflineSignerDirect(chainId: string) {
-    return this.getOfflineSigner(chainId, "direct") as unknown as OfflineDirectSigner;
+    return {
+      getAccounts: async () => {
+        return [await this.getAccount(chainId)];
+      },
+      signDirect: (signerAddress: string, signDoc: SignDoc) =>
+        this.signDirect(chainId, signerAddress, signDoc),
+    };
+  }
+
+  async addChain(chainInfo: ChainRecord) {
+    const suggestChain = chainRegistryChainToCosmostation(
+      chainInfo.chain,
+      chainInfo.assetList ? [chainInfo.assetList] : []
+    );
+    if (chainInfo.preferredEndpoints?.rest?.[0]) {
+      (suggestChain.restURL as string | ExtendedHttpEndpoint) =
+        chainInfo.preferredEndpoints?.rest?.[0];
+    }
+    const result = (await this.cosmos.request({
+      method: 'cos_addChain',
+      params: suggestChain,
+    })) as boolean;
+
+    if (!result) {
+      throw new Error(`Failed to add chain ${chainInfo.name}.`);
+    }
   }
 
   async signAmino(
@@ -81,16 +155,24 @@ export class CosmosExtensionClient implements WalletClient {
     signDoc: StdSignDoc,
     signOptions?: SignOptions
   ) {
-    return await signAmino(chainId, signer, signDoc);
-  }
+    if (this.ikeplr?.signAmino) {
+      return await this.ikeplr.signAmino(
+        chainId,
+        signer,
+        signDoc,
+        signOptions || this.defaultSignOptions
+      );
+    }
 
-  async signArbitrary(
-    chainId: string,
-    signer: string,
-    data: string | Uint8Array
-  ): Promise<StdSignature> {
-    const signature = await this.cosmos.signArbitrary(chainId, signer, data);
-    return signature;
+    return await this.cosmos.request({
+      method: 'cos_signAmino',
+      params: {
+        chainName: chainId,
+        doc: signDoc,
+        isEditMemo: (signOptions || this.defaultSignOptions).preferNoSetMemo,
+        isEditFee: (signOptions || this.defaultSignOptions).preferNoSetFee,
+      },
+    });
   }
 
   async signDirect(
@@ -99,35 +181,62 @@ export class CosmosExtensionClient implements WalletClient {
     signDoc: DirectSignDoc,
     signOptions?: SignOptions
   ): Promise<DirectSignResponse> {
-    if (chainId !== signDoc.chainId) {
-      throw new Error('Chain IDs do not match.');
+    if (this.ikeplr?.signDirect) {
+      const resp = await this.ikeplr.signDirect(
+        chainId,
+        signer,
+        {
+          ...signDoc,
+          accountNumber: Long.fromString(
+            signDoc.accountNumber.toString(),
+            false
+          ),
+        },
+        signOptions || this.defaultSignOptions
+      );
+      return {
+        ...resp,
+        signed: {
+          ...resp.signed,
+          accountNumber: BigInt(resp.signed.accountNumber.toString()),
+        },
+      };
     }
 
-    const { accountNumber } = signDoc;
-
-    const accountNumberLong = new Long(Number(accountNumber.toString()) || 0, Number(accountNumber.toString()), true);
-
-    const newSignDoc: SignDoc = {
-      bodyBytes: signDoc.bodyBytes,
-      authInfoBytes: signDoc.authInfoBytes,
-      chainId: signDoc.chainId,
-      accountNumber: accountNumberLong
-    };
-
-    let signRes = await signDirect(chainId, signer, newSignDoc);
-
-    const sign = {
-      signature: signRes.signature,
-      signed: {
-        ...signRes.signed,
-        accountNumber: accountNumberLong,
-        authInfoBytes: new Uint8Array(
-          Object.values(signRes.signed.authInfoBytes),
-        ),
-        bodyBytes: new Uint8Array(Object.values(signRes.signed.bodyBytes)),
+    return await this.cosmos.request({
+      method: 'cos_signDirect',
+      params: {
+        chainName: chainId,
+        doc: signDoc,
+        isEditMemo: (signOptions || this.defaultSignOptions).preferNoSetMemo,
+        isEditFee: (signOptions || this.defaultSignOptions).preferNoSetFee,
       },
-    };
+    });
+  }
 
-    return sign;
+  async signArbitrary(
+    chainId: string,
+    signer: string,
+    data: string | Uint8Array
+  ): Promise<StdSignature> {
+    try {
+      return await this.ikeplr.signArbitrary(chainId, signer, data);
+    } catch (error) {
+      // https://github.com/cosmostation/cosmostation-chrome-extension-client/blob/main/src/cosmos.ts#LL70C17-L70C28
+      const message =
+        typeof data === 'string' ? data : new TextDecoder('utf-8').decode(data);
+      return await this.cosmos.request({
+        method: 'cos_signMessage',
+        params: {
+          chainName: chainId,
+          signer,
+          message,
+        },
+      });
+    }
+  }
+
+  async sendTx(chainId: string, tx: Uint8Array, mode: BroadcastMode) {
+    return await this.ikeplr.sendTx(chainId, tx, mode);
   }
 }
