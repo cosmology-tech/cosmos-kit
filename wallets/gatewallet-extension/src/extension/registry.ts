1b67df5f50f26107688d5cc0b2f9262ea0f1897f
S_PONY <s_pony@gate.me>
2024-10-17 17:22:45 +0800

    fix: gate wallet info

--- wallets/gatewallet-extension/src/extension/registry.ts
+++ wallets/gatewallet-extension/src/extension/registry.ts
@@ -3,28 +3,18 @@ import { Wallet } from '@cosmos-kit/core';
 import { ICON } from '../constant';
 
 export const GatewalletExtensionInfo: Wallet = {
-  name: 'okxwallet-extension',
-  prettyName: 'OKX Wallet',
+  name: 'gatewallet-extension',
+  prettyName: 'GateWallet',
   logo: ICON,
   mode: 'extension',
   mobileDisabled: true,
   rejectMessage: {
     source: 'Request rejected',
   },
-  connectEventNamesOnWindow: ['okxwallet_keystorechange'],
+  connectEventNamesOnWindow: ['gatewallet_keystorechange'],
   downloads: [
     {
-      device: 'desktop',
-      browser: 'chrome',
-      link: 'https://chrome.google.com/webstore/detail/okx-wallet/mcohilncbfahbmgdjkbpemcciiolgcge',
-    },
-    {
-      device: 'desktop',
-      browser: 'firefox',
-      link: 'https://addons.mozilla.org/zh-CN/firefox/addon/okexwallet/',
-    },
-    {
-      link: 'https://www.okx.com/download',
+      link: 'https://www.gate.io/zh/mobileapp',
     },
   ],
 };
