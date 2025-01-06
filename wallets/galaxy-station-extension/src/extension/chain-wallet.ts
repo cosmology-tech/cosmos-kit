b80ec6fc746fd9153000e19c774ec768d954cdd8
Dan Lynch <pyramation@gmail.com>
2025-01-06 14:41:19 -0800

    temporarily remove wallet missing deps

--- wallets/galaxy-station-extension/src/extension/chain-wallet.ts
+++
@@ -1,7 +0,0 @@
-import { ChainRecord, ChainWalletBase, Wallet } from '@cosmos-kit/core';
-
-export class ChainStationExtension extends ChainWalletBase {
-  constructor(walletInfo: Wallet, chainInfo: ChainRecord) {
-    super(walletInfo, chainInfo);
-  }
-}
--- wallets/galaxy-station/src/index.ts
+++
@@ -1,3 +0,0 @@
-import { wallets as ext } from '@cosmos-kit/galaxy-station-extension';
-
-export const wallets = [...ext];
